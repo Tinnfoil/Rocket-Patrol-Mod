f12bd27d79c02f10ea51633b02c14c93b1a36310
Kenny Doan <kennydoan110999@gmail.com>
2021-04-05 16:42:33 -0700

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Server Command:  py -3 -m http.server
+
+//const Phaser = require("../lib/phaser");
+
+//Game Configuration
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+}
+
+let game = new Phaser.Game(config);
+console.log("Rocket Patrol");
