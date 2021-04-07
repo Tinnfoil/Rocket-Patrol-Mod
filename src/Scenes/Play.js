aab4717f23659f3b62b91e8a98df1847a5854b40
Kenny Doan <kennydoan110999@gmail.com>
2021-04-07 16:58:35 -0700

    Set up

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -11,7 +11,8 @@ class Play extends Phaser.Scene{
 
     create() {
         //place starfield
-        this.starfield = this.add.tileSprite(0,0, 640, 480, 'starfield').setOrigin(0,0);
+        this.starfield = this.add.tileSprite(0,0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
+       
         // Green UI Background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00ff00).setOrigin(0,0);
         this.add.rectangle(0,0, game.config.width, borderUISize, 0xffff0f).setOrigin(0,0);
@@ -19,6 +20,14 @@ class Play extends Phaser.Scene{
         this.add.rectangle(0,0, borderUISize, game.config.height, 0xffff0f).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize,0, borderUISize, game.config.height, 0xffff0f).setOrigin(0,0);
         
-        this.add.text(20,60, "Rocket Patrol Player");
+        this.add.text(20,10, "Rocket Patrol Player");
+        
+        //add the rocket
+        this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5,0);
+   
+    }
+
+    update(){
+        this.starfield.tilePositionX -= starSpeed;
     }
 }
--- src/main.js
+++ src/main.js
@@ -14,3 +14,4 @@ let game = new Phaser.Game(config);
 // Set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
+let starSpeed = 4;
