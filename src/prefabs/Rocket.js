d85f646550bd10b5f6204918916f92bc8a5ccee7
Kenny Doan <kennydoan110999@gmail.com>
2021-04-07 17:09:36 -0700

    Rocket movement

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,5 +5,20 @@ class Rocket extends Phaser.GameObjects.Sprite{
 
         // add object to the existing scene
         scene.add.existing(this);
+        this.isFiring = false;      // Track the firing status
+        this.moveSpeed = 2;         // Pixel per frame
+
+    }
+
+    update(){
+        // left/right movement
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+                this.x -= this.moveSpeed;
+            }
+            if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
+                this.x += this.moveSpeed;
+            }
+        }
     }
 }
