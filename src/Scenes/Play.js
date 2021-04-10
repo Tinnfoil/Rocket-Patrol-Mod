168a51ac0e8f7e7cd25995e82c1ce6f5f89c1cec
Kenny Doan <kennydoan110999@gmail.com>
2021-04-09 17:19:59 -0700

    Explosion

--- src/Scenes/Play.js
+++ src/Scenes/Play.js
@@ -67,15 +67,15 @@ class Play extends Phaser.Scene{
         this.ship03.update();
 
         if(this.checkCollision(this.p1Rocket, this.ship03)){
-            this.ship03.reset();
+            this.shipExplode(this.ship03);
             this.p1Rocket.reset();
         }
         if(this.checkCollision(this.p1Rocket, this.ship02)){
-            this.ship02.reset();
+            this.shipExplode(this.ship02);
             this.p1Rocket.reset();
         }
         if(this.checkCollision(this.p1Rocket, this.ship01)){
-            this.ship01.reset();
+            this.shipExplode(this.ship01);
             this.p1Rocket.reset();
         }
     }
@@ -91,5 +91,18 @@ class Play extends Phaser.Scene{
             return false;
         }
     
+    }
+
+    shipExplode(ship){
+        // Temporarily hide ship
+        ship.alpha = 0;
+        // create explosion spirte at the ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0,0);
+        boom.anims.play('explode');
+        boom.on('animationcomplete', () =>{
+            ship.reset();
+            ship.alpha = 1;
+            boom.destroy();
+        });
     }
 }
